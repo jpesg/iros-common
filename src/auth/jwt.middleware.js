322d0fbfe15b6a12b02a5cdca02bf821293353d0
Michal Tesar <tesarm@gmail.com>
2018-05-03 17:23:15 +0100

    jwt auth - add more details

--- src/auth/jwt.middleware.js
+++ src/auth/jwt.middleware.js
@@ -1,5 +1,6 @@
 import userService from '../service/user';
-import {UnauthorizedHttpError} from '../errors/http.error'
+import {UnauthorizedHttpError} from '../errors/http.error';
+import jsonwebtoken from 'jsonwebtoken';
 
 const authenticate = (role = 'user', section = null) => {
   return (req, res, next) => {
@@ -10,9 +11,12 @@ const authenticate = (role = 'user', section = null) => {
     if (!auth) return next(new UnauthorizedHttpError('Missing Authorization header'));
     if (!auth.match(/^JWT .*$/)) return next(new UnauthorizedHttpError('Only JWT Auth is supported'));
 
-    return userService.canAccess(auth.replace(/^JWT /, ''), role, section)
+    const jwt = auth.replace(/^JWT /, '');
+    const {company} = jsonwebtoken.decode(jwt);
+
+    return userService.canAccess(jwt, role, section)
         .then(u => {
-          req.user = u;
+          req.user = {...u, company, jwt};
           next();
           return null; // bluebird doesn't like it when we return next(), we need to return null to silence this warning
         })
